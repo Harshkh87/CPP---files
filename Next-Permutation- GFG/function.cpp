4d5aa0fab01f486e61582f5ffd73488d9d453a43
harshkh87 <harshkhandelwal073@gmail.com>
2023-07-28 21:25:09 +0530

    first commit

---
+++ Next-Permutation- GFG/function.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+public:
+    vector<int> nextPermutation(int N, vector<int> arr)
+    {
+        next_permutation(arr.begin(),arr.end());
+        return arr;
+    }
+};
